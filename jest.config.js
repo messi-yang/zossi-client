f82f67fa4fdf6f5ed3301cbf88852e177e2952d4
DumDumGenius <dumdumgenius@gmail.com>
2022-02-22 22:45:09 +0800

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -9,7 +9,7 @@ const createJestConfig = nextJest({
 const customJestConfig = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
-  moduleDirectories: ['node_modules', '<rootDir>/'],
+  moduleDirectories: ['<rootDir>/node_modules/', '<rootDir>/.next/'],
   testEnvironment: 'jest-environment-jsdom',
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
