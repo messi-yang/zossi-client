3926c94464785c909f4802148ed8b06bf9b490e4
DumDumGenius <dumdumgenius@gmail.com>
2022-02-22 22:38:45 +0800

    Improve next-i18next.js mockup

--- __mocks__/next-i18next.js
+++ __mocks__/next-i18next.js
@@ -2,11 +2,11 @@ module.exports = {
   // this mock makes sure any components using the translate hook can use it without a warning being shown
   useTranslation: () => {
     return {
-      t: (i18nKey, { ns }) => ns + '.' + i18nKey,
+      t: (i18nKey, { ns }) => (ns ? `${ns}.` : '') + i18nKey,
       i18n: {
         language: 'es',
       },
     };
   },
-  Trans: ({ ns, i18nKey }) => <>{ns + '.' + i18nKey}</>,
+  Trans: ({ ns, i18nKey }) => <>{(ns ? `${ns}.` : '') + i18nKey}</>,
 };
