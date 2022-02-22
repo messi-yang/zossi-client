9769a722df330ff11f093ced1e3c708f5b23e5db
DumDumGenius <dumdumgenius@gmail.com>
2022-02-22 14:23:04 +0800

    Introduce lint-staged

---
+++ .lintstagedrc.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+const buildEslintCommand = (filenames) =>
+  `yarn lint --file ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(' --file ')}`;
+
+const buildJestCommand = (filenames) =>
+  `yarn test --findRelatedTests --bail ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(' ')}`;
+
+module.exports = {
+  '*.{js,jsx,ts,tsx}': [buildEslintCommand, buildJestCommand],
+};
