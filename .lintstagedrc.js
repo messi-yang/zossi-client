49da92559af99d318f7647a996e9e3157e2e940d
Messi Yang <dumdumgenius@gmail.com>
2022-02-26 00:14:55 +0800

    Fix bugs in lintstagedrc.js

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,14 +1,26 @@
 const path = require('path');
 
-const buildEslintCommand = (filenames) =>
-  `yarn lint --file ${filenames
+const isJsFile = (filename) => /(.ts|.tsx|.js|.jsx)$/.test(filename);
+
+const buildEslintCommand = (filenames) => {
+  const jsFiles = filenames.filter(isJsFile);
+  if (jsFiles.length === 0) {
+    return 'echo "No js files"';
+  }
+  return `yarn lint --file ${jsFiles
     .map((f) => path.relative(process.cwd(), f))
     .join(' --file ')}`;
+};
 
-const buildJestCommand = (filenames) =>
-  `yarn test --findRelatedTests --bail ${filenames
+const buildJestCommand = (filenames) => {
+  const jsFiles = filenames.filter(isJsFile);
+  if (jsFiles.length === 0) {
+    return 'echo "No js files"';
+  }
+  return `yarn test --findRelatedTests --bail ${jsFiles
     .map((f) => path.relative(process.cwd(), f))
     .join(' ')}`;
+};
 
 module.exports = {
   './src/**/*': [
