eab5a080f75e793c17f3b060e3f70a7a8e10c484
Messi Yang <dumdumgenius@gmail.com>
2022-02-26 12:16:35 +0800

    Rename hello api to profile

--- src/pages/api/profile.page.ts
+++ src/pages/api/profile.page.ts
@@ -1,13 +1,12 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 type Data = {
-  name: string;
+  nickname: string;
 };
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'Hello World!!!!' });
+  res.status(200).json({ nickname: 'Dynamic Shohei Ohtani' });
 }
