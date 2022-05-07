e9282be09a84d9b989618e57488becb8b382fe37
Messi Yang <dumdumgenius@gmail.com>
2022-05-07 15:23:46 +0800

    1. Add test for useHover hook.

--- src/hooks/useElementResolutionCalculator/index.ts
+++ src/hooks/useElementResolutionCalculator/index.ts
@@ -1,4 +1,5 @@
 import { useState, RefObject, useEffect } from 'react';
+import ResizeObserver from 'resize-observer-polyfill';
 
 export default function useElementResolutionCalculator(
   ref: RefObject<HTMLElement>,
@@ -9,11 +10,12 @@ export default function useElementResolutionCalculator(
 
   useEffect(() => {
     let resizeObserver: ResizeObserver;
-    if (ref.current && window.ResizeObserver) {
-      resizeObserver = new window.ResizeObserver((entries) => {
+    if (ref.current && ResizeObserver) {
+      resizeObserver = new ResizeObserver((entries) => {
         const refCurrentElement = entries[0].target;
-        const elementWidth = refCurrentElement.clientWidth;
-        const elementHeight = refCurrentElement.clientHeight;
+        const boundary = refCurrentElement.getBoundingClientRect();
+        const elementWidth = boundary.width;
+        const elementHeight = boundary.height;
 
         setWidth(Math.floor(elementWidth / unitSize) || 1);
         setHeight(Math.floor(elementHeight / unitSize) || 1);
