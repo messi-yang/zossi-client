ed2f2cffcfd053b8301d38d0ab5657d1b95a3cd2
Messi Yang <dumdumgenius@gmail.com>
2022-07-23 14:27:41 +0800

    feat: Add storybook for GameOfLifeMap.

---
+++ src/components/maps/GameOfLifeMap/index.stories.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import GameOfLifeMap from '.';
+
+export default {
+  title: 'Map/GameOfLifeMap',
+  component: GameOfLifeMap,
+  argTypes: {},
+} as ComponentMeta<typeof GameOfLifeMap>;
+
+const Template: ComponentStory<typeof GameOfLifeMap> = function Template(args) {
+  return (
+    <div style={{ display: 'inline-flex', width: '300px', height: '300px' }}>
+      <GameOfLifeMap {...args} />
+    </div>
+  );
+};
+
+export const Primary = Template.bind({});
+Primary.args = {
+  area: {
+    from: { x: 0, y: 0 },
+    to: { x: 2, y: 2 },
+  },
+  units: [
+    [
+      { alive: false, age: 0 },
+      { alive: false, age: 0 },
+      { alive: false, age: 0 },
+    ],
+    [
+      { alive: false, age: 0 },
+      { alive: false, age: 0 },
+      { alive: false, age: 0 },
+    ],
+    [
+      { alive: false, age: 0 },
+      { alive: false, age: 0 },
+      { alive: false, age: 0 },
+    ],
+  ],
+  relatCoordsForRevival: [{ x: 0, y: 0 }],
+};
