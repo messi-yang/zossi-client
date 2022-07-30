b53f856fb9556056e76dd9ced3dac708cc5cc52c
Messi Yang <dumdumgenius@gmail.com>
2022-07-30 18:50:26 +0800

    refactor: Remove containser beacuse it's not really necessary.

--- src/pages/room/[id].page.tsx
+++ src/pages/room/[id].page.tsx
@@ -1,21 +1,49 @@
 import { useContext, useEffect } from 'react';
 import type { NextPage, GetServerSideProps } from 'next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import debounce from 'lodash/debounce';
 
 import { wrapper } from '@/stores';
 import { getInitialLocale } from '@/utils/i18n';
-import GameOfLifeMapContainer from '@/components/containers/GameOfLifeMapContainer';
-import GameRoomSideBar from '@/components/sidebars/GameRoomSideBar';
+import type { AreaDTO, UnitDTO } from '@/dto';
 import GameOfLibertyContext from '@/contexts/GameOfLiberty';
+import GameRoomSideBar from '@/components/sidebars/GameRoomSideBar';
+import GameOfLifeMap from '@/components/maps/GameOfLifeMap';
+import type { Unit } from '@/components/maps/GameOfLifeMap/types';
+
+function convertGameOfLibertyUnitsToGameOfLifeMapUnits(
+  area: AreaDTO,
+  units: UnitDTO[][]
+): Unit[][] {
+  const { from } = area;
+  return units.map((rowUnits, rowIdx) =>
+    rowUnits.map((unit, colIdx) => ({
+      coordinate: { x: from.x + rowIdx, y: from.y + colIdx },
+      alive: unit.alive,
+      age: unit.age,
+    }))
+  );
+}
 
 const Room: NextPage = function Room() {
   const {
+    area,
+    units,
     status,
     relativeCoordinates,
     joinGame,
     leaveGame,
+    reviveUnits,
+    watchArea,
     updateRelativeCoordinates,
   } = useContext(GameOfLibertyContext);
+
+  const debounceWatchArea = debounce(watchArea, 200);
+
+  const gameFieldUnits = convertGameOfLibertyUnitsToGameOfLifeMapUnits(
+    area,
+    units
+  );
 
   useEffect(() => {
     if (status !== 'ONLINE') {
@@ -38,7 +66,17 @@ const Room: NextPage = function Room() {
         />
       </section>
       <section style={{ flexGrow: '1' }}>
-        <GameOfLifeMapContainer />
+        <section style={{ width: '100%', height: '100%' }}>
+          {status === 'ONLINE' && (
+            <GameOfLifeMap
+              area={area}
+              units={gameFieldUnits}
+              relativeCoordinates={relativeCoordinates}
+              onUnitsRevive={reviveUnits}
+              onAreaUpdate={debounceWatchArea}
+            />
+          )}
+        </section>
       </section>
     </main>
   );
