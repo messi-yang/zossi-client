34b173096407d16e04919fd2308214c746e8afaf
Messi Yang <dumdumgenius@gmail.com>
2022-07-31 20:55:46 +0800

    refactor: Remove unused UnitsPatternDTO.

--- src/dto/UnitsPatternDTO.ts
+++
@@ -1,3 +0,0 @@
-type UnitsPatternDTO = boolean[][];
-
-export default UnitsPatternDTO;
--- src/dto/index.ts
+++ src/dto/index.ts
@@ -2,8 +2,7 @@ import type AreaDTO from './AreaDTO';
 import type CoordinateDTO from './CoordinateDTO';
 import type MapSizeDTO from './MapSizeDTO';
 import type UnitDTO from './UnitDTO';
-import type UnitsPatternDTO from './UnitsPatternDTO';
 
-export { AreaDTO, CoordinateDTO, MapSizeDTO, UnitDTO, UnitsPatternDTO };
+export { AreaDTO, CoordinateDTO, MapSizeDTO, UnitDTO };
 
 export default {};
