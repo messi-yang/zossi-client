c8e378a1d95acaf0a1bc82bb25885a7a8f6229c2
Messi Yang <dumdumgenius@gmail.com>
2022-08-05 22:53:14 +0800

    fix: Fix broken storie of UnitSquare.

--- src/components/squares/UnitSquare/index.stories.tsx
+++ src/components/squares/UnitSquare/index.stories.tsx
@@ -7,36 +7,7 @@ import UnitSquare from '.';
 export default {
   title: 'Square/UnitSquare',
   component: UnitSquare,
-  argTypes: {
-    x: {
-      control: 'number',
-      defaultValue: 10,
-    },
-    y: {
-      control: 'number',
-      defaultValue: 10,
-    },
-    alive: {
-      control: 'boolean',
-    },
-    highlighted: {
-      control: 'boolean',
-    },
-    hasTopBorder: {
-      control: 'boolean',
-    },
-    hasRightBorder: {
-      control: 'boolean',
-    },
-    hasBottomBorder: {
-      control: 'boolean',
-    },
-    hasLeftBorder: {
-      control: 'boolean',
-    },
-    onClick: { action: true },
-    onHover: { action: true },
-  },
+  argTypes: {},
 } as ComponentMeta<typeof UnitSquare>;
 
 const Template: ComponentStory<typeof UnitSquare> = function Template(args) {
@@ -55,4 +26,6 @@ const Template: ComponentStory<typeof UnitSquare> = function Template(args) {
 };
 
 export const Primary = Template.bind({});
-Primary.args = {};
+Primary.args = {
+  coordinate: { x: 0, y: 0 },
+};
