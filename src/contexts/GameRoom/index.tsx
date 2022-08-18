13313ac5364c1d15047f006de53317a2f1021d42
Messi Yang <dumdumgenius@gmail.com>
2022-08-18 16:15:50 +0800

    feat: Add default relaitveCoordinates to GameRoomContext.

--- src/contexts/GameRoom/index.tsx
+++ src/contexts/GameRoom/index.tsx
@@ -44,7 +44,12 @@ function createInitialGameRoomContextValue(): GameRoomContextValue {
     displayedArea: null,
     targetArea: null,
     units: [],
-    relativeCoordinates: null,
+    relativeCoordinates: [
+      { x: -1, y: 0 },
+      { x: 0, y: -1 },
+      { x: 0, y: 1 },
+      { x: 1, y: 0 },
+    ],
     joinGame: () => {},
     updateRelativeCoordinates: () => {},
     reviveUnits: () => {},
