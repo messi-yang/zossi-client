d9cae7ad8573067b8f3ed112a064aafd924bf1ed
Messi Yang <dumdumgenius@gmail.com>
2022-08-29 22:01:00 +0800

    feat: Add unit unitPattern preset

--- src/components/modals/EditUnitPatternModal/unitPatternPresets.ts
+++ src/components/modals/EditUnitPatternModal/unitPatternPresets.ts
@@ -74,6 +74,19 @@ const unitPatternPresets: UnitPatternVO[] = [
     [null, null, true, true, null, null],
     [null, null, null, null, null, null],
   ],
+  [
+    [null, null, null, null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, true, null, null, null, null, null],
+    [null, null, null, null, true, null, true, null, null, null, null],
+    [null, null, null, null, true, null, true, null, null, null, null],
+    [null, null, true, true, null, null, null, true, true, null, null],
+    [null, true, null, null, null, null, null, null, null, true, null],
+    [null, null, true, true, null, null, null, true, true, null, null],
+    [null, null, null, null, true, null, true, null, null, null, null],
+    [null, null, null, null, true, null, true, null, null, null, null],
+    [null, null, null, null, null, true, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null, null, null, null],
+  ],
 ];
 
 export default unitPatternPresets;
