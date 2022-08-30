587f471da38dcfea5108b9c650e6be96e485a7be
Messi Yang <dumdumgenius@gmail.com>
2022-08-30 23:34:01 +0800

    feat: Add new font Silkscreen

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,4 +1,5 @@
 import '@/styles/globals.css';
+import Head from 'next/head';
 import type { AppProps } from 'next/app';
 import { appWithTranslation } from 'next-i18next';
 import { wrapper } from '@/stores';
@@ -7,7 +8,14 @@ import { Provider as GameRoomProvider } from '@/contexts/GameRoom';
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <GameRoomProvider>
-      <Component {...pageProps} />
+      <>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link href="https://fonts.googleapis.com/css2?family=Silkscreen&display=swap" rel="stylesheet" />
+        </Head>
+        <Component {...pageProps} />
+      </>
     </GameRoomProvider>
   );
 }
