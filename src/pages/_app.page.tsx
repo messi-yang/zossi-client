c77ccf1a4ad4468bc9a5a57f3ebc0321e74ae566
Messi Yang <dumdumgenius@gmail.com>
2022-09-10 11:32:53 +0800

    refactor: Move font links to _document.page.tsx and remove all global css

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,5 +1,4 @@
-import '@/styles/globals.css';
-import Head from 'next/head';
+import '@/styles/tailwind.css';
 import type { AppProps } from 'next/app';
 import { appWithTranslation } from 'next-i18next';
 import { wrapper } from '@/stores';
@@ -8,14 +7,7 @@ import { Provider as GameRoomProvider } from '@/contexts/GameRoom';
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <GameRoomProvider>
-      <>
-        <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link href="https://fonts.googleapis.com/css2?family=Silkscreen&display=swap" rel="stylesheet" />
-        </Head>
-        <Component {...pageProps} />
-      </>
+      <Component {...pageProps} />
     </GameRoomProvider>
   );
 }
