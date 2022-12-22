b6041c327b28bef36cf640a3ce50db97886345fc
Messi Yang <dumdumgenius@gmail.com>
2022-12-23 00:12:41 +0800

    refactor: move tailwindcss to pages

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,4 +1,4 @@
-import '@/styles/tailwind.css';
+import './tailwind.css';
 import type { AppProps } from 'next/app';
 import { appWithTranslation } from 'next-i18next';
 import { Provider as GameRoomProvider } from '@/contexts/GameRoom';
