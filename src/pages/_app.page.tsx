8e08ba6db4292ca8e989e08d24b2dc3048b8b601
Messi Yang <dumdumgenius@gmail.com>
2022-12-25 16:37:18 +0800

    refactor: remove unused item context

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -2,15 +2,12 @@ import './tailwind.css';
 import type { AppProps } from 'next/app';
 import { appWithTranslation } from 'next-i18next';
 import { Provider as GameRoomProvider } from '@/contexts/GameContext';
-import { Provider as ItemContextProvider } from '@/contexts/ItemContext';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ItemContextProvider>
-      <GameRoomProvider>
-        <Component {...pageProps} />
-      </GameRoomProvider>
-    </ItemContextProvider>
+    <GameRoomProvider>
+      <Component {...pageProps} />
+    </GameRoomProvider>
   );
 }
 
