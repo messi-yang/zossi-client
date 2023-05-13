ed09ce39f1832b6b481e9faa1d32005400866c53
Messi Yang <dumdumgenius@gmail.com>
2023-05-14 00:44:43 +0800

    fix: make API_PORT env to accept empty string

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const Env = {
   API_SCHEMA: process.env.API_SCHEMA || 'http',
   API_SOCKET_SCHEMA: process.env.API_SOCKET_SCHEMA || 'ws',
   API_HOST_NAME: process.env.API_HOSTNAME || 'localhost',
-  API_PORT: process.env.API_PORT || '8080',
+  API_PORT: process.env.API_PORT !== undefined ? process.env.API_PORT : '8080',
 };
 
 /** @type {import('next').NextConfig} */
