c8b26d23a41bf8a0844f12c4a864e9473f6921d7
Messi Yang <dumdumgenius@gmail.com>
2023-05-14 00:16:03 +0800

    chore: upgrade storybook dependencies

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,18 +1,20 @@
 const path = require('path');
-
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   staticDirs: ['../public'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
-  framework: '@storybook/react',
-  core: {
-    builder: '@storybook/builder-webpack5',
+  framework: {
+    name: '@storybook/nextjs',
+    options: {}
   },
-  webpackFinal: async (config) => {
+  webpackFinal: async config => {
     config.resolve.alias = {
       ...config.resolve.alias,
-      '@': path.resolve(__dirname, '../src/'),
+      '@': path.resolve(__dirname, '../src/')
     };
     return config;
   },
+  docs: {
+    autodocs: true
+  }
 };
