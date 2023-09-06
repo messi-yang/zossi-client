00d4438604f2fbde143422fee6a6f4623d88cce3
Albert Tseng <tjtseng1072@gmail.com>
2023-09-06 23:32:12 +0800

    feat: remove stale logo

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,26 +1,24 @@
 import type { NextPage, GetStaticProps } from 'next';
 import Link from 'next/link';
-import Image from 'next/image';
 
 import { Button } from '@/components/buttons/button';
 
 const Page: NextPage = function Page() {
   return (
     <main className="relative w-screen h-screen flex flex-col items-center justify-center overflow-hidden bg-[#1E1E1E]">
-      <div className="absolute top-0 left-0 w-full h-20 px-16 flex justify-between items-center">
-        <Image src="/assets/images/logos/wordmark-logo.png" alt="wordmark logo" width={270} height={21} />
+      <div className="absolute top-0 left-0 w-full h-20 px-16 flex justify-end items-center">
         <div>
           <Link href="/auth/sign-in" className="flex items-center">
             <Button text="Log In" />
           </Link>
         </div>
       </div>
-      <div className="sm:hidden">
+      {/* <div className="sm:hidden">
         <Image src="/assets/images/logos/big-logo.png" alt="big logo" width={203} height={150} />
       </div>
       <div className="hidden sm:inline-block">
         <Image src="/assets/images/logos/big-logo.png" alt="big logo" width={406} height={231} />
-      </div>
+      </div> */}
       <div className="mt-8 sm:mt-20 flex flex-col items-center">
         <div className="mt-4">
           <Link href="/dashboard/worlds">
