aa6b1cd2645249061813ef3eab3634994ccde3ad
Messi Yang <dumdumgenius@gmail.com>
2024-04-28 15:06:04 +0800

    refactor: add undo mechanism to command

--- src/services/world-journey-service/managers/command-manager/command.ts
+++ src/services/world-journey-service/managers/command-manager/command.ts
@@ -4,9 +4,17 @@ export interface Command {
   getId(): string;
   getTimestamp(): number;
   execute(params: CommandParams): void;
+  /**
+   * Undo the command after execution
+   */
+  undo(): void;
 }
 
+const emptyUndoAction = () => {};
+
 export abstract class BaseCommand implements Command {
+  private undoAction: () => void = emptyUndoAction;
+
   constructor(private id: string, private timestamp: number) {}
 
   public getId() {
@@ -18,4 +26,17 @@ export abstract class BaseCommand implements Command {
   }
 
   public abstract execute(params: CommandParams): void;
+
+  protected getEmptyUndoAction() {
+    return emptyUndoAction;
+  }
+
+  protected setUndoAction(newUndoAction: () => void) {
+    this.undoAction = newUndoAction;
+  }
+
+  public undo(): void {
+    this.undoAction();
+    this.undoAction = emptyUndoAction;
+  }
 }
