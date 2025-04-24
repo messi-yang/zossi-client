42246897ba966c6d0ec260de72ca5633e5fa2a06
Messi Yang <dumdumgenius@gmail.com>
2025-04-25 00:30:01 +0800

    build: fix bundle error caused by useSearchParams

--- src/app/auth/sign-in-success/page.tsx
+++ src/app/auth/sign-in-success/page.tsx
@@ -1,11 +1,11 @@
 'use client';
 
-import { useContext, useEffect } from 'react';
+import { Suspense, useContext, useEffect } from 'react';
 import { useRouter, useSearchParams } from 'next/navigation';
 import { AuthContext } from '@/contexts/auth-context';
 import { Text } from '@/components/texts/text';
 
-const Page = function Page() {
+const PageContent = function PageContent() {
   const router = useRouter();
   const searchParams = useSearchParams();
   const accessToken = searchParams.get('access_token');
@@ -20,10 +20,16 @@ const Page = function Page() {
     router.push(oauthClientRedirectPath);
   }, [accessToken, oauthClientRedirectPath, signIn, router]);
 
+  return <Text>Login Success!</Text>;
+};
+
+const Page = function Page() {
   return (
     <main className="relative w-full h-screen flex justify-center items-center bg-[#1E1E1E]">
       <div className="flex flex-col items-center">
-        <Text>Login Success!</Text>
+        <Suspense fallback={<Text>Loading...</Text>}>
+          <PageContent />
+        </Suspense>
       </div>
     </main>
   );
